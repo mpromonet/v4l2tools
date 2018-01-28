6e0453cb7eac968aac4f5d8be868e5dc83e8c2c7
mpromonet <michel.promonet@free.fr>
2018-01-28 15:56:17 +0000

    add more options to v4l2grab_h264

--- src/v4l2grab_h264.cpp
+++ src/v4l2grab_h264.cpp
@@ -31,12 +31,15 @@ extern "C"
 
 #include "encode_omx.h"
 
-#define NUMFRAMES 300000
+bool stop=false;
+void sighandler(int)
+{ 
+       printf("SIGINT\n");
+       stop = true;
+}
 
-
-int take_snapshot(void *buffer, DISPMANX_DISPLAY_HANDLE_T display, DISPMANX_RESOURCE_HANDLE_T resource, DISPMANX_MODEINFO_T info, VC_RECT_T *rect)
+int take_snapshot(void *buffer, DISPMANX_DISPLAY_HANDLE_T display, DISPMANX_RESOURCE_HANDLE_T resource, DISPMANX_MODEINFO_T info, VC_RECT_T *rect, DISPMANX_TRANSFORM_T transform)
 {
-	DISPMANX_TRANSFORM_T transform = DISPMANX_NO_ROTATE;
 	int ret = 0;
 
 	ret = vc_dispmanx_snapshot(display, resource, transform);
@@ -50,13 +53,50 @@ int take_snapshot(void *buffer, DISPMANX_DISPLAY_HANDLE_T display, DISPMANX_RESO
 
 int main(int argc, char **argv) 
 {
-	if (argc < 2) 
+	const char *out_devname = "/dev/video1";
+	int x = 0;
+	int y = 0;	
+	int width = 0;
+	int height = 0;	
+	V4l2Access::IoType ioTypeOut = V4l2Access::IOTYPE_MMAP;
+	int verbose = 0;
+	
+	int c = 0;
+	while ((c = getopt (argc, argv, "hv::" "X:Y:W:H:" "w")) != -1)
 	{
-		fprintf(stderr, "Usage:\n");
-		fprintf(stderr, "Record to file: %s <filename>\n", argv[0]);
-		exit(1);
+		switch (c)
+		{
+			case 'v':	verbose = 1; if (optarg && *optarg=='v') { verbose++; };  break;
+			case 'X':	x = atoi(optarg); break;
+			case 'Y':	y = atoi(optarg); break;			
+			case 'W':	width = atoi(optarg); break;
+			case 'H':	height = atoi(optarg); break;			
+			case 'w':	ioTypeOut = V4l2Access::IOTYPE_READWRITE; break;	
+			
+			case 'h':
+			{
+				std::cout << argv[0] << " [-v[v]] [-W width] [-H height] source_device dest_device" << std::endl;
+				std::cout << "\t -v            : verbose " << std::endl;
+				std::cout << "\t -vv           : very verbose " << std::endl;
+				
+				std::cout << "\t -X x          : display capture x origin (default "<< x << ")" << std::endl;
+				std::cout << "\t -Y y          : display capture y origin (default "<< y << ")" << std::endl;
+				std::cout << "\t -W width      : display capture width (default "<< width << ")" << std::endl;
+				std::cout << "\t -H height     : display capture height (default "<< height << ")" << std::endl;
+				std::cout << "\t -w            : V4L2 output using write interface (default use memory mapped buffers)" << std::endl;				
+								
+				std::cout << "\t dest_device   : V4L2 output device (default "<< out_devname << ")" << std::endl;
+				exit(0);
+			}
+			
+		}		
 	}
-
+	if (optind<argc)
+	{
+		out_devname = argv[optind];
+		optind++;
+	}	
+	
 	bcm_host_init();
 	
 	uint32_t                    screen = 0;
@@ -73,81 +113,85 @@ int main(int argc, char **argv)
 	ret = vc_dispmanx_display_get_info(display, &info);
 	assert(ret == 0);
 	fprintf(stderr, "Display is %d x %d\n", info.width, info.height );
+	
+	if (width == 0) {
+		width = info.width;
+	}
+	if (height == 0) {
+		height = info.height;
+	}
 
 	uint32_t                    vc_image_ptr;
 	DISPMANX_RESOURCE_HANDLE_T resource = vc_dispmanx_resource_create( VC_IMAGE_BGR888, info.width, info.height, &vc_image_ptr);
 
 	VC_RECT_T                   rect;
-	ret = vc_dispmanx_rect_set(&rect, 0, 0, info.width, info.height);
+	ret = vc_dispmanx_rect_set(&rect, x, y, width, height);
 	assert(ret == 0);
 	
-	int verbose=0;
-
 	// initialize log4cpp
 	initLogger(verbose);
 	
-	V4l2Access::IoType ioTypeOut = V4l2Access::IOTYPE_MMAP;
-	V4L2DeviceParameters outparam(argv[1], V4L2_PIX_FMT_H264,  info.width, info.height, 0, true);
+	V4L2DeviceParameters outparam(out_devname, V4L2_PIX_FMT_H264,  width, height, 0, verbose);
 	V4l2Output* videoOutput = V4l2Output::create(outparam, ioTypeOut);
 	if (videoOutput == NULL)
 	{	
 		LOG(WARN) << "Cannot create V4L2 output interface for device:" << argv[1]; 
+		status = -1;
 	}
 	else
 	{ 
-		OMX_BUFFERHEADERTYPE *      buf = NULL;
-		OMX_BUFFERHEADERTYPE *      out = NULL;
 		COMPONENT_T *               video_encode = NULL;
 
 		ILCLIENT_T *client = encode_init(&video_encode);
 		if (client)
 		{
-			encode_config_input(video_encode, info.width, info.height, 30, OMX_COLOR_Format24bitBGR888);
+			encode_config_input(video_encode, width, height, 30, OMX_COLOR_Format24bitBGR888);
 			encode_config_output(video_encode, OMX_VIDEO_CodingAVC, 10000000);
 
 			encode_config_activate(video_encode);
 
 			fprintf(stderr, "looping for buffers...\n");
-			do
+			
+			while (!stop)
 			{
-				buf = ilclient_get_input_buffer(video_encode, 200, 0);
+				OMX_BUFFERHEADERTYPE *buf = ilclient_get_input_buffer(video_encode, 200, 0);
 				if (buf != NULL)
 				{
 					/* fill it */
-					buf->nFilledLen = take_snapshot(buf->pBuffer, display, resource, info, &rect);
+					buf->nFilledLen = take_snapshot(buf->pBuffer, display, resource, info, &rect, DISPMANX_NO_ROTATE);
 					framenumber++;
 
 					if (OMX_EmptyThisBuffer(ILC_GET_HANDLE(video_encode), buf) != OMX_ErrorNone)
 					{
-						fprintf(stderr, "Error emptying buffer!\n");
+						LOG(WARN) << "Error emptying buffer"; 
 					}
 				}
 					
-				out = ilclient_get_output_buffer(video_encode, 201, 0);
+				OMX_BUFFERHEADERTYPE *out = ilclient_get_output_buffer(video_encode, 201, 0);
 				if (out != NULL)
 				{
 					OMX_ERRORTYPE r = OMX_FillThisBuffer(ILC_GET_HANDLE(video_encode), out);
 					if (r != OMX_ErrorNone)
 					{
-						fprintf(stderr, "Error filling buffer: %x\n", r);
+						LOG(WARN) << "Error filling buffer:" << r ; 
 					}
 					if (out->nFilledLen > 0)
 					{
 						size_t sz = videoOutput->write((char*)out->pBuffer, out->nFilledLen);
 						if (sz != out->nFilledLen)
 						{
-							fprintf(stderr, "fwrite: Error emptying buffer: %d!\n", sz);
+							LOG(WARN) << "fwrite: Error emptying buffer:" << sz << "!=" << out->nFilledLen; 
 						}
 						else
 						{
-	//						fprintf(stderr, "Writing frame size:%d %d/%d\n", sz ,framenumber, NUMFRAMES);
+							LOG(DEBUG) << "Writing frame size:" << sz; 
 						}
 					}
 					
 					out->nFilledLen = 0;
 				}
 
-			} while (framenumber < NUMFRAMES);
+			} 
 			
 			fprintf(stderr, "Exit\n");
 		
